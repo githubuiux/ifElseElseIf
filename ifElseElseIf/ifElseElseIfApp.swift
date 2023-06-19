1d4a65922cd8e308ed0f736a0111c75c8b46c148
githubuiux <uiuxista@gmail.com>
2023-06-19 13:06:51 +0300

    Initial Commit

---
+++ ifElseElseIf/ifElseElseIfApp.swift
@@ -0,0 +1,17 @@
+//
+//  ifElseElseIfApp.swift
+//  ifElseElseIf
+//
+//  Created by Levent on 19.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ifElseElseIfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
